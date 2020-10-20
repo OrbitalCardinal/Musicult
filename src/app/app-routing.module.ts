a9ac617ba963af31e21f6ebbd6647a957d4f5512
Edson Raul Cepeda Marquez <44615729+OrbitalCardinal@users.noreply.github.com>
2020-10-19 20:43:40 -0500

    Routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MainViewComponent } from './main-view/main-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { NowPlayingBarComponent } from './now-playing-bar/now-playing-bar.component';
 
 const routes: Routes = [
-  { path: '', component: MainViewComponent}
+  { path: '', component: MainViewComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'now-playing', component: NowPlayingBarComponent }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
